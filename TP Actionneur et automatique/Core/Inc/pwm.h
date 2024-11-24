a407c74636bd577f1a39c566512d5466c269b60a
RafikCH16 <chair.rafik@gmail.com>
2024-11-24 13:49:49 +0100

    Commit 24/11/2024 CODE

---
+++ TP Actionneur et automatique/Core/Inc/pwm.h
@@ -0,0 +1,20 @@
+/*
+ * pwm.h
+ *
+ *  Created on: Oct 24, 2022
+ *      Author:
+ */
+
+#ifndef INC_PWM_H_
+#define INC_PWM_H_
+
+#include <stdio.h>
+#include <stdint.h>
+
+void start_command(uint8_t alpha);
+void stop_command();
+void change_speed(uint8_t alpha);
+void change_speed_progressively(uint8_t alpha);
+void read_current();
+void start_up();
+#endif /* INC_PWM_H_ */
